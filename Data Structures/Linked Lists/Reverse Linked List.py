2e037f0498a0dbb1ef9e16db2524bed31a6abaea
laksh394 <63038314+laksh394@users.noreply.github.com>
2024-09-20 23:45:46 +0530

    Create Reverse Linked List.py

---
+++ Data Structures/Linked Lists/Reverse Linked List.py
@@ -0,0 +1,21 @@
+"""Q2. Reverse Linked List
+Given the head of a singly linked list, reverse the list, and return the reversed list."""
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Initialize pointers
+        prev = None  # Previous node starts as None
+        curr = head  # Current node starts at the head
+
+        # Traverse the list
+        while curr is not None:
+            next_node = curr.next  # Save the next node
+            
+            curr.next = prev  # Reverse the link
+            
+            # Move pointers forward
+            prev = curr  # Move prev to the current node
+            curr = next_node  # Move curr to the next node
+
+        # prev is now the new head of the reversed list
+        return prev
