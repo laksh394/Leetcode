6a8e198fbebce9dd41b547383883fa0fb3fb4ae1
laksh394 <63038314+laksh394@users.noreply.github.com>
2024-09-20 23:47:51 +0530

    Update Reverse Linked List.py

--- Data Structures/Linked Lists/Reverse Linked List.py
+++ Data Structures/Linked Lists/Reverse Linked List.py
@@ -1,5 +1,10 @@
 """Q2. Reverse Linked List
-Given the head of a singly linked list, reverse the list, and return the reversed list."""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+"""
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
