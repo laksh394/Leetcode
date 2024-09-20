2dd74ef580b3a0a45dc9f17a45ba8e9be2dc1f25
laksh394 <63038314+laksh394@users.noreply.github.com>
2024-09-20 23:54:09 +0530

    Update Remove Linked List Elements.py

--- Data Structures/Linked Lists/Remove Linked List Elements.py
+++ Data Structures/Linked Lists/Remove Linked List Elements.py
@@ -1,4 +1,4 @@
-"""Q1. Remove Linked List Elements
+"""Remove Linked List Elements
 Given the head of a linked list and an integer val, remove all the nodes of the linked list that has 
 Node.val == val, and return the new head.
 
