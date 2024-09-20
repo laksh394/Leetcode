a87d948e421ecbc16ab77d987a105e921c2d3af9
laksh394 <63038314+laksh394@users.noreply.github.com>
2024-09-20 23:20:51 +0530

    Update 1) Remove Linked List Elements.py

--- DataStructures/Linked Lists/1) Remove Linked List Elements.py
+++ DataStructures/Linked Lists/1) Remove Linked List Elements.py
@@ -1,22 +1,10 @@
-Q1. Remove Linked List Elements
-Before writing any code, it's good to make a list of edge cases that we need to consider. This is so that we can be certain that we're not overlooking anything while coming up with our algorithm, and that we're testing all special cases when we're ready to test. These are the edge cases that I came up with.
-
-The linked list is empty, i.e. the head node is None.
-Multiple nodes with the target value in a row.
-The head node has the target value.
-The head node, and any number of nodes immediately after it have the target value.
-All of the nodes have the target value.
-The last node has the target value.
-So with that, this is the algorithm I came up with.
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def removeElements(self, head, val):
-        """
-        :type head: ListNode
-        :type val: int
-        :rtype: ListNode
-        """
-        
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         dummy_head = ListNode(-1)
         dummy_head.next = head
         
@@ -28,12 +16,27 @@ class Solution:
                 current_node = current_node.next
                 
         return dummy_head.next
-In order to save the need to treat the "head" as special, the algorithm uses a "dummy" head. This simplifies the code greatly, particularly in the case of needing to remove the head AND some of the nodes immediately after it.
+"""
+Q1. Remove Linked List Elements
+Before writing any code, it's good to make a list of edge cases that we need to consider. This is so that we can be certain that we 
+are not overlooking anything while coming up with our algorithm, and that we're testing all special cases when we're ready to test. These are the edge cases that I came up with.
 
-Then, we keep track of the current node we're up to, and look ahead to its next node, as long as it exists. If current_node.next does need removing, then we simply replace it with current_node.next.next. We know this is always "safe", because current_node.next is definitely not None (the loop condition ensures that), so we can safely access its next.
+The linked list is empty, i.e. the head node is None.
+Multiple nodes with the target value in a row.
+The head node has the target value.
+The head node, and any number of nodes immediately after it have the target value.
+All of the nodes have the target value.
+The last node has the target value.
 
-Otherwise, we know that current_node.next should be kept, and so we move current_node on to be current_node.next.
+In order to save the need to treat the "head" as special, the algorithm uses a "dummy" head. This simplifies the code greatly, particularly 
+in the case of needing to remove the head AND some of the nodes immediately after it.
+Then, we keep track of the current node we're up to, and look ahead to its next node, as long as it exists. If current_node.next does need 
+removing, then we simply replace it with current_node.next.next. We know this is always "safe", because current_node.next is definitely not 
+None (the loop condition ensures that), so we can safely access its next. Otherwise, we know that current_node.next should be kept, and so 
+we move current_node on to be current_node.next.
 
-The loop condition only needs to check that current_node.next != None. The reason it does not need to check that current_node != None is because this is an impossible state to reach. Think about it this way: The ONLY case that we ever do current_node = current_node.next in is immediately after the loop has already confirmed that current_node.next is not None.
+The loop condition only needs to check that current_node.next != None. The reason it does not need to check that current_node != None is 
+because this is an impossible state to reach. Think about it this way: The ONLY case that we ever do current_node = current_node.next in is immediately after the loop has already confirmed that current_node.next is not None.
 
-The algorithm requires O(1) extra space and takes O(n) time.
+The algorithm requires O(1) extra space and takes O(n) time."""
+
