93048a4c1e5f435fc64323b576d23751ed977f45
Lakshmi Vijay <63038314+laksh394@users.noreply.github.com>
2025-04-14 12:58:52 +0530

    Create Get the Size of a DataFrame(Pandas).py

---
+++ Pandas or Polars/Get the Size of a DataFrame(Pandas).py
@@ -0,0 +1,7 @@
+# Write a solution to calculate and display the number of rows and columns of players.
+# Return the result as an array: [number of rows, number of columns]
+
+import pandas as pd
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return [players.shape[0], players.shape[1]]
