82f8427acd628ab5d0c0ea11f1113bd661782ebc
Lakshmi Vijay <63038314+laksh394@users.noreply.github.com>
2025-04-14 13:10:07 +0530

    Create Display first three rows(Pandas).py

---
+++ Pandas or Polars/Display first three rows(Pandas).py
@@ -0,0 +1,15 @@
+# DataFrame: employees
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | employee_id | int    |
+# | name        | object |
+# | department  | object |
+# | salary      | int    |
+# +-------------+--------+
+# Write a solution to display the first 3 rows of this DataFrame.
+
+import polars as pl
+
+def selectFirstRows(employees: pl.DataFrame) -> pl.DataFrame:
+    return employees.head(3)
