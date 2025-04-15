c992189aca54d3a95f595363e70419826a05728b
Lakshmi Vijay <63038314+laksh394@users.noreply.github.com>
2025-04-15 19:56:43 +0530

    Update Fill Missing Data(Pandas).py

--- Pandas or Polars/Fill Missing Data(Pandas).py
+++ Pandas or Polars/Fill Missing Data(Pandas).py
@@ -13,3 +13,6 @@ import pandas as pd
 def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
     products["quantity"] = products["quantity"].fillna(0)
     return products
+
+# For all columns
+# products = products.fill_na(0)
